935c26625f1b5e02c382de18f9f90b2c123c4f9d
eliseomagana <63011935+eliseomagana@users.noreply.github.com>
2022-04-19 10:58:26 -0400

    Up to Task 21

--- WawaStore/Startup.cs
+++ WawaStore/Startup.cs
@@ -9,17 +9,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WawaStore.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace WawaStore
 {
     public class Startup
     {
+        IConfigurationRoot Configuration;
+        public Startup(IWebHostEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json").Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<IProductRepository, FakeProductRepository>();
 
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
