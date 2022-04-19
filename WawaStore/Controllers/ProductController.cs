a63086205daee5601e0a2606c757bee7005e22d7
JuanJimene <n01375595@unf.edu>
2022-04-18 23:51:10 -0400

    Updated files and added database, next will be working on the html views of it, start at "26. Task 25: Adding the view model" - juan

--- WawaStore/Controllers/ProductController.cs
+++ WawaStore/Controllers/ProductController.cs
@@ -10,8 +10,13 @@ namespace WawaStore.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
         public ProductController(IProductRepository repo)
         { repository = repo; }
-        public ViewResult List() => View(repository.Products);
+        public ViewResult List(int page = 1) => View(repository.Products
+                                                .OrderBy(p => p.ProductID)
+                                                .Skip((page - 1) * PageSize)
+                                                .Take(PageSize));
+        
     }
 }
--- WawaStore/Startup.cs
+++ WawaStore/Startup.cs
@@ -9,33 +9,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WawaStore.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace WawaStore
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public class Startup
+        {
+            IConfigurationRoot Configuration;
+            public Startup(IWebHostEnvironment env)
+            {
+                Configuration = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json").Build();
+            }
+
+            // This method gets called by the runtime. Use this method to add services to the container.
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940 
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddTransient<IProductRepository, FakeProductRepository>();
+            {
+                //services.AddTransient<IProductRepository, FakeProductRepository>(); 
 
-            services.AddMvc(options => options.EnableEndpointRouting = false);
-        }
+                services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                services.AddTransient<IProductRepository, EFProductRepository>();
+                services.AddMvc(options => options.EnableEndpointRouting = false);
+            }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory
-loggerFactory)
-        {
+            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory
+    loggerFactory)
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+                app.UseStaticFiles();
+                app.UseMvc(routes => {
+                    routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Product}/{action=List}/{id?}");
+                });
 
-            app.UseDeveloperExceptionPage();
-            app.UseStatusCodePages();
-            app.UseStaticFiles();
-            app.UseMvc(routes => {
-                routes.MapRoute(
-                name: "default",
-                template: "{controller=Product}/{action=List}/{id?}");
-            });
+            }
         }
     }
-}
